b56fe405badc7916e625f7788388aea990e8e7a8
Shahriar Hossain <shahriar.hossain03@northsouth.edu>
2020-11-17 21:40:38 +0600

    Homework 1

---
+++ Homework Submission/Homework 1/1.asm
@@ -0,0 +1,16 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+MOV  AX, 1
+ADD  AX, 2
+ADD  AX, 3
+MOV  CX,AX
+
+ret
+
+
+
+
