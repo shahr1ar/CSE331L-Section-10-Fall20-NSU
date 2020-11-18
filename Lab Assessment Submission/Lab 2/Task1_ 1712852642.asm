1b52997088898ce35346af96e6c2f9ed56af0e94
Shahriar Hossain <shahriar.hossain03@northsouth.edu>
2020-11-18 09:28:25 +0600

    lab 2

---
+++ Lab Assessment Submission/Lab 2/Task1_ 1712852642.asm
@@ -0,0 +1,22 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+K1 EQU 1 
+K2 EQU 1
+K3 EQU 1
+
+
+ADD AX,K1
+
+ADD AX,K2
+ADD AX,K3
+MOV BX,AX
+
+ret
+
+
+
+
